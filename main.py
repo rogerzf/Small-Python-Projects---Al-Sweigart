c26377b659881fa6cf16dcaef2800a0969ae2074
Dennis Roger Ferreira <rogerzf@live.com>
2022-11-12 15:56:24 -0300

    1st commit

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 # 12/11/2022
 #
-# Programmin is a fun, creative skill to develop.
+# Programming is a fun, creative skill to develop.
 # Whether you've mastered the basics os Python syntax or simply want to
 # dive into some real Python programs, the projects
 # in this book will spark new ideas for what's possible with as little
 # as a few pages of code.
 #     The best way to work through these programs isn't to merely
-# read theis code or copy and past it to your computer. Take the time
+# read the code or copy and paste it to your computer. Take the time
 # to manually type the code from this book into your editor
-# to develop the muscle memory of wwriting code. This also slows you
-# down so you can considr each line as you type, instead of merely
+# to develop the muscle memory of writing code. This also slows you
+# down so you can consider each line as you type, instead of merely
 # skimming it over with your eyes. Look up any instructions you don't
 # recognize using an internet search engine, or experiment
-# with them in the interactive sheel.
-#     Finally, take it upn yourself to re-create the
-# program from scratch and ten modifu it features of your own.
+# with them in the interactive shell.
+#     Finally, take it up yourself to re-create the
+# program from scratch and then modify it features of your own.
 # These exercises give you a solid foundation for how
 # programming concepts are applied to create actual, runnable programs.
-# And moost of all, don't forget to have fun"
+# And most of all, don't forget to have fun!
