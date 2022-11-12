97f3cfee35044aa804f316c26f563df807a3d7a2
Dennis Roger Ferreira <rogerzf@live.com>
2022-11-12 15:53:54 -0300

    1st commit

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 12/11/2022
+#
+# Programmin is a fun, creative skill to develop.
+# Whether you've mastered the basics os Python syntax or simply want to
+# dive into some real Python programs, the projects
+# in this book will spark new ideas for what's possible with as little
+# as a few pages of code.
+#     The best way to work through these programs isn't to merely
+# read theis code or copy and past it to your computer. Take the time
+# to manually type the code from this book into your editor
+# to develop the muscle memory of wwriting code. This also slows you
+# down so you can considr each line as you type, instead of merely
+# skimming it over with your eyes. Look up any instructions you don't
+# recognize using an internet search engine, or experiment
+# with them in the interactive sheel.
+#     Finally, take it upn yourself to re-create the
+# program from scratch and ten modifu it features of your own.
+# These exercises give you a solid foundation for how
+# programming concepts are applied to create actual, runnable programs.
+# And moost of all, don't forget to have fun"
